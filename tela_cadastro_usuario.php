e1bc52cf4a38186503bf918155452531476ce63d
Crisciany <criscianysilva1997@gmail.com>
2024-07-16 17:53:26 -0400

    criação tela de cadastro de usuário
    Signed-off-by: Crisciany <criscianysilva1997@gmail.com>

---
+++ tela_cadastro_usuario.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>Sistema PHP</title>
+		<meta name="description" content="Free Bootstrap 4 Admin Theme | Pike Admin">
+		<meta name="author" content="Pike Web Development - https://www.pikephp.com">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="/Nura-Admin/src/assets/images/favicon.ico">
+
+		<!-- Bootstrap CSS -->
+		<link href="/Nura-Admin/src/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		
+		<!-- Font Awesome CSS -->
+		<link href="/Nura-Admin/src/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		
+		<!-- Custom CSS -->
+		<link href="/Nura-Admin/src/assets/css/style.css" rel="stylesheet" type="text/css" />
+		
+		<!-- BEGIN CSS for this page -->
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
+		<!-- END CSS for this page -->
+		
+</head>
+
+<body>
+
+<div class="container mt-5">
+    <h3 class="text-center">Cadastre seus dados abaixo: </h3>
+    <div class="row justify-content-md-center">
+        <div class="col-lg-6 p-5 text-center">
+            <img src="./src/img/pidge_icon.jpg" alt="imagem_tela_login" style="border-radius: 150px;">
+        </div>
+    </div>
+    <div class="row justify-content-md-center">
+
+        <div class="col-lg-6 p-5">
+            <form>
+                <!-- Email input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="email" id="form2Example1" class="form-control" />
+                    <label class="form-label" for="form2Example1">Email</label>
+                </div>
+                
+                <!-- Password input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="form2Example2" class="form-control" />
+                    <label class="form-label" for="form2Example2">Senha</label>
+                </div>
+                
+                <!-- Password input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="form2Example2" class="form-control" />
+                    <label class="form-label" for="form2Example2">Confirmar Senha</label>
+                </div>
+                
+                <!-- Submit button -->
+                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Salvar</button>
+
+            </form>
+        </div>
+    </div>
+
+</div>
+</body>
+
+<!-- <script src="/assets/js/modernizr.min.js"></script> -->
+<script src="/Nura-Admin/src/assets/js/modernizr.min.js"></script>
+<script src="/Nura-Admin/src/assets/js/jquery.min.js"></script>
+<script src="/Nura-Admin/src/assets/js/moment.min.js"></script>
+		
+<script src="/Nura-Admin/src/assets/js/popper.min.js"></script>
+<script src="/Nura-Admin/src/assets/js/bootstrap.min.js"></script>
+
+<script src="/Nura-Admin/src/assets/js/detect.js"></script>
+<script src="/Nura-Admin/src/assets/js/fastclick.js"></script>
+<script src="/Nura-Admin/src/assets/js/jquery.blockUI.js"></script>
+<script src="/Nura-Admin/src/assets/js/jquery.nicescroll.js"></script>
+
+</html>
